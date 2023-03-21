99ad6b6fdc0d78a5f002b20ebde338210ded72f7
Jakub Delicat <jakub.delicat@husarion.com>
2023-03-21 15:00:26 +0100

    removed empty line

--- include/main.hpp
+++ include/main.hpp
@@ -56,7 +56,6 @@ std::map<double, uint8_t> servo_voltage_configuration{
 #define MOTOR_RL MOTOR3
 time_t last_button_press_time[BUTTONS_COUNT];
 
-
 constexpr uint8_t POLARITY = 0b00111100;
 constexpr float ROBOT_LENGTH = 0.197;
 constexpr uint8_t ENCODER_CPR = 48;
