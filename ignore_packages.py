58c75c938329923a9b8722da901fcef59cf66c19
Jakub Delicat <jakub.delicat@husarion.com>
2023-06-27 12:46:59 +0200

    created folder for ignore

--- ignore_packages.py
+++ ignore_packages.py
@@ -1,3 +1,10 @@
+import os
+
+try:
+    os.makedirs('/home/husarion/.platformio/packages/framework-mbed/features/')
+except FileExistsError:
+    print('Folder already exists')
+
 with open('/home/husarion/.platformio/packages/framework-mbed/features/.mbedignore', 'w') as f:
     f.write('cellular/*\n')
     f.write('netsocket/cellular/*\n')
