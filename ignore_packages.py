1f43b637e5f403d5797d3622210c4c17f4a95cca
Jakub Delicat <jakub.delicat@husarion.com>
2023-06-27 13:03:07 +0200

    changed user in devcontainer

--- ignore_packages.py
+++ ignore_packages.py
@@ -1,11 +1,11 @@
 import os
 
 try:
-    os.makedirs('/home/husarion/.platformio/packages/framework-mbed/features/')
+    os.makedirs('/home/runner/.platformio/packages/framework-mbed/features/')
 except FileExistsError:
     print('Folder already exists')
 
-with open('/home/husarion/.platformio/packages/framework-mbed/features/.mbedignore', 'w') as f:
+with open('/home/runner/.platformio/packages/framework-mbed/features/.mbedignore', 'w') as f:
     f.write('cellular/*\n')
     f.write('netsocket/cellular/*\n')
     f.write('nanostack/*\n')
