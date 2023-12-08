537af1b1dd6070ba56da16c66a239253f674aa0f
Dominik Nowak <dominik.nwk@gmail.com>
2023-12-08 17:57:51 +0100

    Pc connection (#24)
    Detecting whether Micro ROS Agent is connected over an SBC pin header (RaspberryPi 4, o UpBoard) or over the USB-B on the rear panel.

--- include/main.hpp
+++ include/main.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <microros.hpp>
 
-static UARTSerial microros_serial(RPI_SERIAL_TX, RPI_SERIAL_RX);
+static UARTSerial microros_serial_rpi(RPI_SERIAL_TX, RPI_SERIAL_RX);
+static UARTSerial microros_serial_ftdi(FT_SERIAL_TX, FT_SERIAL_RX);
 
 static volatile bool distance_sensors_enabled = false;
 static DigitalOut sens_power(SENS_POWER_ON, 0);
